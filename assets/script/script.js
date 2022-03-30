e6d4657ef212c77468be1eea70aa60848330aae8
Haaomas <barreauji@gmail.com>
2022-03-30 15:07:45 +0200

    Tweaking and clean code
    Clean HTML
    Clean CSS (add color)
    Clean JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,14 +4,14 @@ console.log("---------");
 
 //Début du code
 //Variables
-//Time
-
-//Div
+//Div hand
 const hoursDiv = document.querySelector('.hour-hand');
 const minutesDiv = document.querySelector('.main-hand');
 const secondesDiv = document.querySelector('.second-hand');
 
+
 function affichage() {
+    //Time
     let date = new Date;
     let hours = date.getHours();
     hours = (hours % 12) || 12;
@@ -21,23 +21,10 @@ function affichage() {
     let secondes = date.getSeconds();
     secondes = secondes * 6 + 90;
 
+    //Transforme on the hand
     hoursDiv.style.transform = `rotate(${hours}deg)`;
     minutesDiv.style.transform = `rotate(${minutes}deg)`;
     secondesDiv.style.transform = `rotate(${secondes}deg)`;
 }
+//Interval of 1 seconde
 setInterval(affichage, 1000);
-
-
-
-
-/*
-    * Pseudo Code Créer une fonction qui détermine la position de chaque aiguille en fonction de l'heure 
-    * Au chargement de la page ajouter une classe qui contient du style dynamique pour chaque aiguille  
-    * 
-    * Créer un timer qui fait augmenter le nombre de degré de cette classe à chaque seconde
-
-/* 
-    * Nombre de degrés par seconde pour seconde = 6 
-    * Nombre de degré par seconde pour minutes 0.1
-    * Nombre de degré par seconde pour heures 0.0083
-*/
